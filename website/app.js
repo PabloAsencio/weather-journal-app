c055b6592a6b57cd4e0734ed156a85793eeb6f46
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-10-27 16:12:50 +0100

    feat: Add basic functionality

--- website/app.js
+++ website/app.js
@@ -1,5 +1,85 @@
 /* Global Variables */
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Fetch local temperature for the given zip Code from openweathermap.org
+const fechtWeatherData = async (baseURL, zipCode, key) => {
+    const response = await fetch(
+        baseURL + zipCode + ',de&appid=' + key + '&units=metric'
+    );
+    try {
+        const data = await response.json();
+        return {
+            temperature: data.main.temp,
+        };
+    } catch (error) {
+        return {
+            errorMessage:
+                'Ooops! We had a problem retrieving the data.\nPlease, try again!',
+        };
+    }
+};
+
+// Post a journal entry to the server
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Fetch the last journal entry from the server and update the UI with it
+const updateUI = async () => {
+    console.log('Entering updateUI');
+    const temp = document.getElementById('temp');
+    const date = document.getElementById('date');
+    const content = document.getElementById('content');
+
+    const response = await fetch('/lastEntry', {
+        method: 'GET',
+        credentials: 'same-origin',
+    });
+    try {
+        const lastEntry = await response.json();
+        temp.innerHTML = lastEntry.temperature;
+        date.innerHTML = lastEntry.date;
+        content.innerHTML = lastEntry.userResponse;
+    } catch (error) {
+        console.log('error');
+    }
+};
+
+// Callback to post a new journal entry to the server and update the UI with it
+const updateLastEntry = () => {
+    const zipCode = document.getElementById('zip').value;
+    const userResponse = document.getElementById('feelings').value;
+    fechtWeatherData(baseURL, zipCode, apiKey).then((data) => {
+        postData('/addData', {
+            temperature: data.temperature,
+            date: newDate,
+            userResponse: userResponse,
+        }).then(() => {
+            updateUI();
+        });
+    });
+    document.getElementById('zip').value = '';
+    document.getElementById('feelings').value = '';
+};
+
+// Set event listeners
+document.getElementById('generate').addEventListener('click', updateLastEntry);
